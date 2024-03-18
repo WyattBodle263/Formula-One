db7d54f5533405e8fae9978328a2d2717a9b58ac
Nahum Pina <nahum.pina@calbaptist.edu>
2024-03-18 10:47:12 -0700

    All code, needs img and debug

--- src/FormulaOne.java
+++ src/FormulaOne.java
@@ -60,8 +60,8 @@ public class FormulaOne {
         p1width = 228;
         p1height = 228;
         cockpitShift = 350;
-        p1origionalX = (double) XOFFSET + ((double) WINWIDTH / 2.0) - (p1width / 2.0);
-        p1origionalY = (double) YOFFSET + (double) cockpitShift;
+        p1originalX = (double) XOFFSET + ((double) WINWIDTH / 2.0) - (p1width / 2.0);
+        p1originalY = (double) YOFFSET + (double) cockpitShift;
 
         trackMatrix = new Vector<Vector<Vector<Integer>>>();
 
@@ -70,7 +70,7 @@ public class FormulaOne {
             player = ImageIO.read(new File("steeringWheelSmall.png"));
             cockpit = ImageIO.read(new File("cockpit.png"));
             track = ImageIO.read(new File("trackchessboard.png"));
-            perspectiveTrack = convertToARGB(ImageIO.read(new File("perspectiveTrack.png")))
+            perspectiveTrack = convertToARGB(ImageIO.read(new File("perspectiveTrack.png")));
 
 
         } catch (IOException e) {
@@ -131,7 +131,7 @@ public class FormulaOne {
                     if (p1velocity < 0) {
                         p1.rotate(rotatestep);
                     } else {
-                        p1.roatate(-rotatestep);
+                        p1.rotate(-rotatestep);
                     }
                 }
             }
@@ -202,7 +202,7 @@ public class FormulaOne {
 
     private static AffineTransformOp rotateImageObject(ImageIO obj) {
         AffineTransform at = AffineTransform.getRotateInstance(-obj.getAngle(), obj.getWidth() / 2.0, obj.getheight() / 2.0);
-        AffineTransformOp atop = new AffineTransformOp((at, AffineTransformOp.TYPE_BILINEAR));
+        AffineTransformOp atop = new AffineTransformOp(at, AffineTransformOp.TYPE_BILINEAR);
         return atop;
     }
 
@@ -258,8 +258,8 @@ public class FormulaOne {
     }
 
     private static Vector< Vector< Vector< Integer > > > rotateImage ( Vector < Vector< Vector< Integer > > > inputImg , double angle , double xpos , double ypos , boolean repeatImg ) {
-        Vector< Vector< Vector< Integer > > > ret = new Vector< Vector< Vector< Integer > > >() ;
-        for( int i = 0; i < inputImg.size(); i++ ) {
+        Vector<Vector<Vector<Integer>>> ret = new Vector<Vector<Vector<Integer>>>();
+        for (int i = 0; i < inputImg.size(); i++) {
             Vector<Vector<Integer>> tempRow = new Vector<Vector<Integer>>();
             for (int j = 0; j < inputImg.elementAt(i).size(); j++) {
                 Vector<Integer> tempPixel = new Vector<Integer>();
@@ -271,23 +271,405 @@ public class FormulaOne {
             }
             ret.addElement(tempRow);
         }
-        for(int i = 0; i < inputImg.size(); i++){
-            for(int j = 0; j < inputImg.elementAt(i).size(); j++){
-                int newj = (int)(0.5 + xpos + ((double)j - xpos) * Math.cos(angle) - ((double)i - ypos) * Math.sin(angle));
-                int newi = (int)(0.5 + ypos + ((double)j - xpos) * Math.sin(angle) + ((double)i - ypos) * Math.cos(angle));
+        for (int i = 0; i < inputImg.size(); i++) {
+            for (int j = 0; j < inputImg.elementAt(i).size(); j++) {
+                int newj = (int) (0.5 + xpos + ((double) j - xpos) * Math.cos(angle) - ((double) i - ypos) * Math.sin(angle));
+                int newi = (int) (0.5 + ypos + ((double) j - xpos) * Math.sin(angle) + ((double) i - ypos) * Math.cos(angle));
 
-                if(repeatImg){
-                    while (newj >= ret.elementAt())
+                if (repeatImg) {
+                    while (newj >= ret.elementAt(0).size()) {
+                        newj = newj - ret.elementAt(0).size();
+                    }
+                    while (newj < 0) {
+                        newj = newj + ret.elementAt(0).size();
+                    }
+                    while (newi >= ret.size()) {
+                        newi = newi - ret.size();
+                    }
+                    while (newi < 0) {
+                        newi = newi + ret.size();
+                    }
+                }
+
+                if (newj < ret.elementAt(0).size() && newj >= 0) {
+                    if (newi < ret.size() && newi >= 0) {
+                        ret.elementAt(newi).elementAt(newj).set(0, inputImg.elementAt(i).elementAt(j).elementAt(0));
+                        ret.elementAt(newi).elementAt(newj).set(1, inputImg.elementAt(i).elementAt(j).elementAt(1));
+                        ret.elementAt(newi).elementAt(newj).set(2, inputImg.elementAt(i).elementAt(j).elementAt(2));
+                    }
                 }
             }
         }
+        return ret;
+    }
 
+    private static Vector<Vector<Vector<Integer>>> duplicate3x3(Vector<Vector<Vector<Integer>>> inputImg){
+        Vector<Vector<Vector<Integer>>> ret = new Vector<Vector<Vector<Integer>>>();
+        for(int i = 0; i < inputImg.size() * 3; i++){
+            Vector<Vector<Integer>> tempRow = new Vector<Vector<Integer>>();
+            for(int j = 0; j < inputImg.size() * 3; i++){
+                Vector<Integer> tempPixel = new Vector<Integer>();
+                tempPixel.addElement(0);
+                tempPixel.addElement(0);
+                tempPixel.addElement(0);
 
+                tempRow.addElement(tempPixel);
+            }
+            ret.addElement(tempRow);
+        }
+        for(int i = 0; i < ret.size(); i++){
+            for(int j = 0; j < ret.elementAt(i).size(); j++){
+                ret.elementAt(i).elementAt(j).set(0, inputImg.elementAt(i % inputImg.size()).elementAt(j % inputImg.elementAt(0).size()).elementAt(0));
+                ret.elementAt(i).elementAt(j).set(1, inputImg.elementAt(i % inputImg.size()).elementAt(j % inputImg.elementAt(0).size()).elementAt(1));
+                ret.elementAt(i).elementAt(j).set(2, inputImg.elementAt(i % inputImg.size()).elementAt(j % inputImg.elementAt(0).size()).elementAt(2));
+            }
+        }
+        return ret;
+    }
+
+    private static void trackDraw(){
+        int rectWidth = 500;
+        int rectHeight = 175;
+        int base = 150;
+        int xoffset = 0;
+        int yoffset = 232;
+        int scaledown = 5;
+
+        Vector<Vector<Vector<Integer>>> cameraView = new Vector<Vector<Vector<Integer>>>();
+        for(int i = 0; i < rectHeight; i++){
+            Vector<Vector<Integer>> tempRow = new Vector<Vector<Integer>>();
+            for(int j = 0; j < rectWidth; j++){
+                Vector<Integer> tempRGB = new Vector<Integer>();
+
+                int indexi = cameray - (rectHeight - i);
+                int indexj = camerax - (rectWidth - j + (int)(0.5 + ((double)rectWidth/2.0)));
+
+                while(indexi < 0){
+                    indexi = indexi + trackMatrix.size();
+                }
+                while(trackMatrix.size() <= indexi){
+                    indexi = indexi - trackMatrix.size();
+                }
+                while(indexj < 0){
+                    indexj = indexj + trackMatrix.elementAt(0).size();
+                }
+                while(trackMatrix.elementAt(0).size() < indexj){
+                    indexj = indexj - trackMatrix.elementAt(0).size();
+                }
+
+                tempRGB.addElement(trackMatrix.elementAt(indexi).elementAt(indexj).elementAt(0));
+                tempRGB.addElement(trackMatrix.elementAt(indexi).elementAt(indexj).elementAt(1));
+                tempRGB.addElement(trackMatrix.elementAt(indexi).elementAt(indexj).elementAt(2));
+
+                tempRow.addElement(tempRGB);
+            }
+
+            cameraView.addElement(tempRow);
+        }
+
+        Vector<Vector<Vector<Integer>>> userView = perspectiveFromRectangle(cameraView, base);
+
+        Graphics g = appFrame.getGraphics();
+        Graphics2D g2D = (Graphics2D) g;
+
+        for(int i = 0; i < rectHeight; i++){
+            for(int j = 0; j < rectWidth; j++){
+                int alpha = 255;
+                int red = userView.elementAt(i).elementAt(j).elementAt(0);
+                int green = userView.elementAt(i).elementAt(j).elementAt(1);
+                int blue = userView.elementAt(i).elementAt(j).elementAt(2);
+
+                while(red < 0){
+                    red += 256;
+                }
+                while(256 <= red){
+                    red -= 256;
+                }
+                while(green < 0){
+                    green += 256;
+                }
+                while(256 <= green){
+                    green -= 256;
+                }
+                while(blue < 0){
+                    blue += 256;
+                }
+                while(256 <= blue){
+                    blue -= 256;
+                }
+
+                Color myColor = new Color(red, green, blue);
+                int rgb = myColor.getRGB();
+                perspectiveTrack.setRGB(j,i,rgb);
+            }
+        }
+        g2D.drawImage(perspectiveTrack, XOFFSET, YOFFSET + yoffset, null);
+    }
+
+    private static void playerDraw(){
+        Graphics g = appFrame.getGraphics();
+        Graphics2D g2D = (Graphics2D) g;
+        g2D.drawImage(cockpit, XOFFSET, cockpitShift, null);
+        g2D.drawImage(rotateImageObject(p1).filter(player, null), (int)(p1.getX() + 0.5), (int)(p1.getY() + 0.5), null);
+    }
+
+private static class KeyPressed extends AbstractAction{
+        public KeyPressed(){
+            action = "";
+        }
+        public KeyPressed(String input){
+            action = input;
+        }
+
+        public void actionPerformed(ActionEvent e){
+            if(action.equals("UP")){
+                upPressed = true;
+            }
+            if(action.equals("DOWN")){
+                downPressed = true;
+            }
+            if(action.equals("LEFT")){
+                leftPressed = true;
+            }
+            if(action.equals("RIGHT")){
+                rightPressed = true;
+            }
+        }
+        private String action;
+}
+
+private static class KeyReleased extends AbstractAction{
+    public KeyReleased(){
+        action = "";
+    }
+    public KeyReleased(String input){
+        action = input;
+    }
+
+    public void actionPerformed(ActionEvent e){
+        if(action.equals("UP")){
+            upPressed = false;
+        }
+        if(action.equals("DOWN")){
+            downPressed = false;
+        }
+        if(action.equals("LEFT")){
+            leftPressed = false;
+        }
+        if(action.equals("RIGHT")){
+            rightPressed = false;
+        }
+    }
+    private String action;
+}
+
+private static class QuitGame implements ActionListener{
+        public void actionPerformed(ActionEvent e){
+            endgame = true;
+        }
+}
+
+private static class StartGame implements ActionListener{
+        public void actionPerformed(ActionEvent e){
+            endgame = true;
+            upPressed = false;
+            downPressed = false;
+            leftPressed = false;
+            rightPressed = false;
+
+            p1 = new ImageObject(p1originalX, p1originalY, p1width, p1height, 0,0);
+            p1velocity = 0.0;
+            camerax = 0;
+            cameray = 0;
+
+            try{
+                Thread.sleep(50);
+            } catch(InterruptedException ie){}
+
+            setUpTrack();
+            endgame = false;
+            Thread t1 = new Thread(new Animate());
+            Thread t2 = new Thread(new PlayerMover());
+            Thread t3 = new Thread(new CameraMover());
+
+            t1.start();
+            t2.start();
+            t3.start();
+        }
+}
+
+private static class ImageObject{
+        public ImageObject(){
+        }
+
+        public ImageObject(double xinput, double yinput, double xwidthinput, double yheightinput, double angleinput){
+            x = xinput;
+            y = yinput;
+            xwidth = xwidthinput;
+            yheight = yheightinput;
+            angle = angleinput;
+            angle = angleinput;
+            internalangle = 0.0;
+            coords = new Vector<Double>();
+        }
+
+        public double getX(){
+            return x;
+        }
+
+        public double getY(){
+            return y;
+        }
+
+        public double getWidht(){
+            return xwidth;
+        }
+
+        public double getHeight(){
+            return yheight;
+        }
+
+        public double getAngle(){
+            return angle;
+        }
+
+        public double getInternalAngle(){
+            return internalangle;
+        }
+
+        public void setAngle(double angleinput) {
+            angle = angleinput;
+        }
+        public void setInternalAngle(double internalangleinput){
+            internalangle = internalangleinput;
+        }
+
+        public Vector<Double> getCoords(){
+            return coords;
+        }
+
+        public void setCoords(Vector<Double> coordsinput){
+            coords = coordsinput;
+            generateTriangles();
+        }
+
+        public void generateTriangles(){
+            triangles = new Vector<Double>();
+
+            comX = getComX();
+            comY = getComY();
+
+            for (int i = 0; i < coords.size(); i = i + 2) {
+                triangles.addElement(coords.elementAt(i));
+                triangles.addElement(coords.elementAt(i + 1));
+
+                triangles.addElement(coords.elementAt((i + 2) % coords.size()));
+                triangles.addElement(coords.elementAt((i + 3) % coords.size()));
+
+                triangles.addElement(comX);
+                triangles.addElement(comY);
+            }
+        }
+
+        public void printTriangles(){
+            for(int i = 0; i < triangles.size(); i = i + 6){
+                System.out.print("p0x");
+                System.out.print("p1x");
+            }
+        }
+
+        public double getComX(){
+            double ret = 0;
+            if(coords.size() > 0){
+                for(int i = 0; i < coords.size(); i = i + 2){
+                    ret = ret + coords.elementAt(i);
+                }
+                ret = ret / (coords.size() / 2.0);
+            }
+            return ret;
+        }
+
+        public double getComY(){
+            double ret = 0;
+            if(coords.size() > 0){
+                for(int i = 0; i < coords.size(); i = i + 2){
+                    ret = ret + coords.elementAt(i);
+                }
+                ret = ret / (coords.size() / 2.0);
+            }
+            return ret;
+        }
+
+        public void move(double xinput, double yinput) {
+            x = x + xinput;
+            y = y + yinput;
+        }
+        public void moveto(double xinput, double yinput){
+            x = xinput;
+            y = yinput;
+        }
+
+        public void screenWrap(double leftEdge, double rightEdge, double topEdge, double bottomEdge){
+            if(x > rightEdge){
+                moveto(leftEdge, getY());
+            }
+            if(x < leftEdge){
+                moveto(rightEdge, getY());
+            }
+            if(y > bottomEdge){
+                moveto(getX(), topEdge);
+            }
+            if(y < topEdge){
+                moveto(getX(), bottomEdge);
+            }
+        }
+
+        public void rotate(double angleinput){
+            angle = angle + angleinput;
+            while(angle > twoPi){
+                angle = angle - twoPi;
+            }
+
+            while(angle < 0){
+                angle = angle + twoPi;
+            }
+        }
+
+        public void spin(double internalangleinput){
+            internalangle = internalangle + internalangleinput;
+            while(internalangle > twoPi){
+                internalangle = internalangle - twoPi;
+            }
+
+            while(internalangle < 0){
+                internalangle = internalangle + twoPi;
+            }
+        }
+
+        private double x;
+        private double y;
+        private double xwidth;
+        private double yheight;
+        private double angle;
+        private double internalangle;
+        private double comX;
+        private double comY;
+        private Vector<Double> coords;
+        private Vector<Double> triangles;
+}
+
+private static void bindKey(JPanel myPanel, String input){
+        myPanel.getInputMap(IFW).put(KeyStroke.getKeyStroke("pressed " + input), input + " pressed");
+        myPanel.getActionMap().put(input + " pressed", new KeyPressed(input));
+
+        myPanel.getInputMap(IFW).put(KeyStroke.getKeyStroke("released " + input), input + " released");
+        myPanel.getActionMap().put(input + " released", new KeyReleased(input));
+}
 
     public static void main(String[] args){
         setup();
         appFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        appFreame.setSize(501,585);
+        appFrame.setSize(501,585);
 
         JPanel myPanel = new JPanel();
 
@@ -330,8 +712,8 @@ public class FormulaOne {
     private static ImageObject p1;
     private static double p1width;
     private static double p1height;
-    private static double p1origionalX;
-    private static double p1origionalY;
+    private static double p1originalX;
+    private static double p1originalY;
     private static double p1velocity;
 
     private static int XOFFSET;
